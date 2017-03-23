78b01c0851a24a4fdac2839b1d54ab8156125253
aquairus <18650013469@163.com>
2017-03-23 13:55:48 +0800

    test

--- experiments/examples/VGG_CNN_S/train_vgg_s_rap_0.sh
+++ experiments/examples/VGG_CNN_S/train_vgg_s_rap_0.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd `dirname "${BASH_SOURCE[0]}"`/../../..
-./experiments/scripts/wpal_net.sh 1 VGG_S_MLL_RAP ./data/snapshots/VGG_S_MLL_RAP/0/RAP/vgg_s_mll_rap_iter_5000.caffemodel  RAP 0 0 3 --set TRAIN.BATCH_SIZE 54
+./experiments/scripts/wpal_net.sh 1 VGG_S_MLL_RAP data/pretrained/VGG_CNN_S.caffemodel   RAP 0 0 3 --set TRAIN.BATCH_SIZE 54
