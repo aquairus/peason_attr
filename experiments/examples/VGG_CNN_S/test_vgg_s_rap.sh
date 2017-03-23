4e6e2729428ad82bc82e7fc9f938c0447b56faf8
aquairus <18650013469@163.com>
2017-03-23 16:13:43 +0800

    test

--- experiments/examples/VGG_CNN_S/test_vgg_s_rap.sh
+++ experiments/examples/VGG_CNN_S/test_vgg_s_rap.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd `dirname "${BASH_SOURCE[0]}"`/../../..
-./tools/test_net.py --setid 0 --net ./data/snapshots/VGG_S_MLL_RAP/0/attr0_3/vgg_s_mll_rap_iter_100.caffemodel --start 0 --end 3 --def ./models/VGG_S_MLL_RAP/test_net.prototxt --gpu 1 --db RAP
+./tools/test_net.py --setid 0 --net ./data/snapshots/VGG_S_MLL_RAP/0/attr0_3/RAP/vgg_s_mll_rap_iter_100.caffemodel --start 0 --end 3 --def ./models/VGG_S_MLL_RAP/test_net.prototxt --gpu 1 --db RAP
