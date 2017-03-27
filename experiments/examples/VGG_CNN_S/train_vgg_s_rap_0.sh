254316d77dc6adc9346281453934c4e64df4c28f
aquairus <18650013469@163.com>
2017-03-28 00:51:13 +0800

    attr

--- experiments/examples/VGG_CNN_S/train_vgg_s_rap_0.sh
+++ experiments/examples/VGG_CNN_S/train_vgg_s_rap_0.sh
@@ -2,4 +2,4 @@
 cd `dirname "${BASH_SOURCE[0]}"`/../../..
 start=$1
 end=$2
-./experiments/scripts/wpal_net.sh 0 VGG_S_MLL_RAP data/pretrained/VGG_CNN_S.caffemodel   RAP 0 start end --set TRAIN.BATCH_SIZE 45
+./experiments/scripts/wpal_net.sh 0 VGG_S_MLL_RAP data/pretrained/VGG_CNN_S.caffemodel   RAP 0 ${start} ${end} --set TRAIN.BATCH_SIZE 45
