70086418749e5dd01bbcb6634966ff8ffd18948c
aquairus <18650013469@163.com>
2017-03-27 23:39:58 +0800

    attr

--- experiments/examples/VGG_CNN_S/train.py
+++ experiments/examples/VGG_CNN_S/train.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
+
 import sys
 import os
 
 
 items=[[1,4],[4,7],[15,24],[24,30],[30,35],[34,51],[63,75],[75,83],[83,92]]
 for item in items:
-    os.system('./train_vgg_s_rap_0.sh {0} {1}'.format(item[0],item(1))
+    os.system('./train_vgg_s_rap_0.sh {0} {1}'.format(item[0],item[1])
